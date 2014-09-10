e98f26fc587ba2dc77c38467104b7db9f648f93d
Mikhail S. Pobolovets <styx.mp@gmail.com>
2014-09-10 16:38:32 +0300

    Update Elixir version
    Signed-off-by: Mikhail S. Pobolovets <styx.mp@gmail.com>

--- lib/word.ex
+++ lib/word.ex
@@ -6,17 +6,45 @@ defmodule Word do
 
   @doc """
 
+  Converts the word to the list of tokens
+  of size n by sequential splitting on each
+  char
+
   Example:
 
   iex> Word.seq_split "word", 2
   ["wo", "or", "rd"]
 
+  iex> Word.seq_split "тест", 3
+  ["тес", "ест"]
+
+  iex> Word.seq_split "ox", 2
+  ["ox"]
+
+  iex> Word.seq_split "x", 2
+  []
+
   """
 
+  def seq_split(string, n) do
+    if (string |> String.length) >= n do
+      seq_split(string |> to_char_list, n, [])
+    else
+      []
+    end
+  end
 
-  def seq_split([]), do: []
-
-  def seq_split() do
+  defp seq_split([], _, acc), do: acc |> Enum.reverse
+  defp seq_split(chars, n, acc) do
+    len = chars |> length
+    cond do
+      len == n ->
+        seq_split([], n, [chars |> to_string | acc])
+      true ->
+        tail = chars |> Enum.drop(1)
+        chunk = chars |> Enum.take(n) |> to_string
+        seq_split(tail, n, [chunk | acc])
+    end
   end
 
 end
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Word.Mixfile do
   def project do
     [app: :word,
      version: "0.0.1",
-     elixir: "~> 0.15.1",
+     elixir: "~> 1.0",
      deps: deps]
   end
 
