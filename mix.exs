2aa1a322660cc45b9c2fd0d124fc48d237e31998
Mikhail S. Pobolovets <styx.mp@gmail.com>
2014-11-30 09:19:12 +0300

    Update deps
    Signed-off-by: Mikhail S. Pobolovets <styx.mp@gmail.com>

--- mix.exs
+++ mix.exs
@@ -41,9 +41,9 @@ defmodule Word.Mixfile do
     [
       {:earmark, "~> 0.1", only: :dev},
       {:ex_doc, "~> 0.6", only: :dev},
-      {:mock, "~> 0.1.0", only: :test},
+      {:mock, "~> 0.1", only: :test},
       {:postgrex, ">= 0.0.0"},
-      {:ecto, "~> 0.2.4"}
+      {:ecto, "~> 0.2.5"}
     ]
   end
 end
